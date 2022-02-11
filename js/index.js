952e3af5c35eb241229e54bd9148b941483b8184
nicb112 <91611250+nicb112@users.noreply.github.com>
2022-02-11 18:57:23 +0800

    testing

--- js/index.js
+++ js/index.js
@@ -3,8 +3,9 @@ var mainVideo = $('video');
 if ($(window).width() < 800) {
     //   document.getElementById('video').classList.add('behind');
     //   splash.classList.add('display-none')
-    document.getElementsByClassName('splash')[0].style.display = "none";
-    document.getElementsByClassName('splash').style.zIndex = "-10";
+    mainVideo.append("<source type='video/mp4' src='video/Logo Animation (Portrait).mp4' />");
+    // document.getElementsByClassName('splash')[0].style.display = "none";
+    // document.getElementsByClassName('splash').style.zIndex = "-10";
 
 } else {
     // document.getElementsByClassName('splash')[0].style.zIndex = "200";

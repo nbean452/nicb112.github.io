bd762975c0c411d3fb99b8ed968b45ebc23b004b
nicb112 <91611250+nicb112@users.noreply.github.com>
2022-02-28 19:32:21 +0800

    almost fin

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-var splashAnimation = document.querySelector('#splash-animation');
-var splash = document.querySelector('.splash');
+var splashAnimation = document.getElementById("splash-animation");
+var splash = document.getElementById("splash");
 
 //when splash animation ends, calls another function to hide the splash animation
 splashAnimation.addEventListener("animationend", () => {
+    console.log("anim ended!");
+    
     setTimeout(fadeOutSplash, 2000);
 });
 
 function fadeOutSplash(){
-    splash.classList.add('display-none');
+    splash.style.zIndex = "-100";
+    splash.style.opacity = "0";
 }
